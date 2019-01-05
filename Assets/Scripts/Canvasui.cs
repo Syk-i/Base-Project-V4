d3d8a85d1ecc5e93ea3b0bbd32c3143966a4788a
Syk-i <34940095+Syk-i@users.noreply.github.com>
2019-01-05 10:20:40 +0300

    Loading screen wokring

--- Assets/Scripts/Canvasui.cs
+++ Assets/Scripts/Canvasui.cs
@@ -9,7 +9,7 @@ public class Canvasui : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         
-        uidrop.SetActive(false);
+        uidrop.SetActive(true);
     }
 	
 	// Update is called once per frame
