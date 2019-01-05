026f527417b86fc714e8f1c7d12397917dd2fc9a
Syk-i <34940095+Syk-i@users.noreply.github.com>
2019-01-05 10:17:45 +0300

    Initial commit

---
+++ Assets/Scripts/Canvasui.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Canvasui : MonoBehaviour {
+   public  GameObject uidrop;
+   
+	// Use this for initialization
+	void Start () {
+        
+        uidrop.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+	}
+}
